2df71e579362a501bf9630a0cba02b020164213e
Atul kumar <atul993418@gmail.com>
2025-06-22 19:05:57 +0530

    preparing backend to upload on render

---
+++ Backend/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI , File , UploadFile , Form
+from DocxWriter import write_to_docx
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+    "https://file-maker-college.vercel.app",
+    "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def get_hello () :
+    return {"response" : "Hello world"}
+
+FileRoute = str(os.getenv('FILE_ROUTE'))
+if not FileRoute:
+    raise ValueError("FILE_ROUTE environment variable is not set.")
+
+@app.post(FileRoute)
+async def processFile (filename:str = Form(...) ,file:UploadFile = File(...),language:str = Form(...) , codeFontSize: int = Form(...) , questionFontSize: int = Form(...)): 
+    content = await file.read()
+    modifiedFile = await write_to_docx(content,codeFontSize,questionFontSize,language)
+    return StreamingResponse(
+        modifiedFile ,
+        media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        headers={"Content-Disposition": f"attachment; filename={filename}.docx"}
+    )
